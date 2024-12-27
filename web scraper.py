ed709f0b42213b0878635e062c4f0c352bee717f
ZohaibM-Use <113934174+ZohaibM-Use@users.noreply.github.com>
2024-12-27 18:35:59 -0500

    Add files via upload

---
+++ web scraper.py
@@ -0,0 +1,29 @@
+import time
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+from bs4 import BeautifulSoup
+
+driver_path = "C:/Users/zohai/OneDrive/Documents/Class 37/Mod 3/CIS 403/Week 17/msedgedriver.exe"  # Replace with the actual path
+
+service = Service(driver_path)
+driver = webdriver.Edge(service=service)
+
+url = "https://oxylabs.io/blog"
+driver.get(url)
+
+results = []
+content = driver.page_source
+soup = BeautifulSoup(content)
+driver.quit()
+
+headings = soup.find_all(['p'])
+
+for heading in headings:
+    title = heading.get_text(strip=True)
+    if title not in results:
+        results.append(title)
+
+df = pd.DataFrame({'All Text': results})
+df.to_csv('text.csv', index=False, encoding='utf-8')
